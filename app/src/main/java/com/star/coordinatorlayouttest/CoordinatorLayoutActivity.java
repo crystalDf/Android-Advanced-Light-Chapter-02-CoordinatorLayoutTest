5586cd0cfd41fb6661c5cdfdd7320cf21f821d52
crystalDf <chendong333@gmail.com>
2018-08-01 22:28:19 +0800

    CoordinatorLayoutTest

--- app/src/main/java/com/star/coordinatorlayouttest/CoordinatorLayoutActivity.java
+++ app/src/main/java/com/star/coordinatorlayouttest/CoordinatorLayoutActivity.java
@@ -1,13 +1,112 @@
 package com.star.coordinatorlayouttest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CoordinatorLayoutActivity extends AppCompatActivity {
+
+    private ViewPager mViewPager;
+    private TabLayout mTabLayout;
+    private Toolbar mToolbar;
+
+    private ActionBar mActionBar;
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mNavigationView;
+
+    private FragmentAdapter mFragmentAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coordinator_layout);
+
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        mActionBar = getSupportActionBar();
+        mActionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        mActionBar.setDisplayHomeAsUpEnabled(true);
+
+        mViewPager = findViewById(R.id.viewpager);
+        initViewPager();
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mNavigationView = findViewById(R.id.nav_view);
+
+        if (mNavigationView != null) {
+
+            mNavigationView.setNavigationItemSelectedListener(item -> {
+
+                item.setChecked(true);
+
+                String title = item.getTitle().toString();
+
+                Toast.makeText(getApplicationContext(), title, Toast.LENGTH_LONG).show();
+
+                mDrawerLayout.closeDrawers();
+
+                return true;
+            });
+        }
+    }
+
+    private void initViewPager() {
+
+        mTabLayout = findViewById(R.id.tab_layout);
+
+        List<String> titles = new ArrayList<>();
+        List<Fragment> fragments = new ArrayList<>();
+
+        titles.add("精选");
+        titles.add("体育");
+        titles.add("巴萨");
+        titles.add("购物");
+        titles.add("明星");
+        titles.add("视频");
+        titles.add("健康");
+        titles.add("励志");
+        titles.add("图文");
+        titles.add("本地");
+        titles.add("动漫");
+        titles.add("搞笑");
+        titles.add("精选");
+
+        for (String title : titles) {
+
+            mTabLayout.addTab(mTabLayout.newTab().setText(title));
+            fragments.add(new ListFragment());
+        }
+
+        mFragmentAdapter = new FragmentAdapter(
+                getSupportFragmentManager(), fragments, titles);
+
+        mViewPager.setAdapter(mFragmentAdapter);
+        mTabLayout.setupWithViewPager(mViewPager);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
