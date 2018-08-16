7608a65451a8aea0d1fd3856bde23ffa63aaca07
crystalDf <chendong333@gmail.com>
2018-08-16 21:57:16 +0800

    CoordinatorLayoutTest

---
+++ app/src/main/java/com/star/coordinatorlayouttest/CollapsingToolbarActivity.java
@@ -0,0 +1,35 @@
+package com.star.coordinatorlayouttest;
+
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.support.v7.widget.Toolbar;
+
+public class CollapsingToolbarActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private CollapsingToolbarLayout mCollapsingToolbarLayout;
+    private RecyclerView mRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collapsing_toolbar_layout);
+
+        mToolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(mToolbar);
+
+        mCollapsingToolbarLayout = findViewById(R.id.collapsing_toolbar_layout);
+        mCollapsingToolbarLayout.setTitle("机器猫");
+
+        mRecyclerView = findViewById(R.id.recycler_view);
+        mRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(
+                1, StaggeredGridLayoutManager.VERTICAL));
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        mRecyclerView.setAdapter(new RecyclerViewAdapter(CollapsingToolbarActivity.this));
+    }
+}
