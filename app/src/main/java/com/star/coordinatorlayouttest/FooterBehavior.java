1f479ad60bdf5af8e5c9151193bc6ce6bf2ea570
crystalDf <chendong333@gmail.com>
2018-08-20 23:40:43 +0800

    CoordinatorLayoutTest

---
+++ app/src/main/java/com/star/coordinatorlayouttest/FooterBehavior.java
@@ -0,0 +1,7 @@
+package com.star.coordinatorlayouttest;
+
+import android.support.design.widget.CoordinatorLayout;
+import android.view.View;
+
+public class FooterBehavior extends CoordinatorLayout.Behavior<View> {
+}
