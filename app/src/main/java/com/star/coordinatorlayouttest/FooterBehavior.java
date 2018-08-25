ef232f48db48f4832a7205fe196dd2267bc86ab3
crystalDf <chendong333@gmail.com>
2018-08-25 20:59:23 +0800

    CoordinatorLayoutTest

--- app/src/main/java/com/star/coordinatorlayouttest/FooterBehavior.java
+++ app/src/main/java/com/star/coordinatorlayouttest/FooterBehavior.java
@@ -3,6 +3,7 @@ package com.star.coordinatorlayouttest;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.view.ViewCompat;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -14,7 +15,7 @@ public class FooterBehavior extends CoordinatorLayout.Behavior<View> {
 
     @Override
     public boolean onStartNestedScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull View child, @NonNull View directTargetChild, @NonNull View target, int axes, int type) {
-        return super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target, axes, type);
+        return (axes & ViewCompat.SCROLL_AXIS_VERTICAL) != 0;
     }
 
     @Override
