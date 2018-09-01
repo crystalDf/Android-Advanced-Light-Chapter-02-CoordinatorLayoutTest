b6604a1d81f592044d2069800aaeadf35265c43a
crystalDf <chendong333@gmail.com>
2018-09-02 01:42:33 +0800

    CoordinatorLayoutTest

--- app/src/main/java/com/star/coordinatorlayouttest/FooterBehaviorAppBar.java
+++ app/src/main/java/com/star/coordinatorlayouttest/FooterBehaviorAppBar.java
@@ -1,6 +1,8 @@
 package com.star.coordinatorlayouttest;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CoordinatorLayout;
 import android.util.AttributeSet;
 import android.view.View;
@@ -9,5 +11,10 @@ public class FooterBehaviorAppBar extends CoordinatorLayout.Behavior<View> {
 
     public FooterBehaviorAppBar(Context context, AttributeSet attrs) {
         super(context, attrs);
+    }
+
+    @Override
+    public boolean layoutDependsOn(@NonNull CoordinatorLayout parent, @NonNull View child, @NonNull View dependency) {
+        return dependency instanceof AppBarLayout;
     }
 }
