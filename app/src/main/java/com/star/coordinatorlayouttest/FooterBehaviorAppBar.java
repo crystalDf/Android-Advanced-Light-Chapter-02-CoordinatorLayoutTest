eb4bbb0e3d772366599f8f35600f72dfb9aa44fe
crystalDf <chendong333@gmail.com>
2018-09-01 23:03:35 +0800

    CoordinatorLayoutTest

---
+++ app/src/main/java/com/star/coordinatorlayouttest/FooterBehaviorAppBar.java
@@ -0,0 +1,13 @@
+package com.star.coordinatorlayouttest;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class FooterBehaviorAppBar extends CoordinatorLayout.Behavior<View> {
+
+    public FooterBehaviorAppBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
